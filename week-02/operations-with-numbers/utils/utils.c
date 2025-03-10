17272aaea373c72dd885959921049a0ac28839f3
TBalint9 <tokoli.balint@student.ms.sapientia.ro>
2025-03-10 08:09:59 +0200

    utils

--- week-02/operations-with-numbers/utils/utils.c
+++ week-02/operations-with-numbers/utils/utils.c
@@ -3,3 +3,11 @@
 //
 
 #include "utils.h"
+#include <stdio.h>
+
+void printArray(int *arr, int size) {
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
